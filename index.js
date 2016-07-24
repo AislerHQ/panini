28d6780815c62e076c60df8b1666efae74fd244d
Patrick Franken <p.franken@aisler.net>
2016-07-24 19:28:36 +0200

    Rename from panini to panini-mustache

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var panini;
  * @constructor
  * @param {object} options - Configuration options to use.
  */
-function Panini(options) {
+function PaniniMustache(options) {
   this.options = options;
   this.Mustache = require('mustache');
   this.layouts = {};
@@ -21,11 +21,11 @@ function Panini(options) {
   }
 }
 
-Panini.prototype.refresh = require('./lib/refresh');
-Panini.prototype.loadLayouts = require('./lib/loadLayouts');
-Panini.prototype.loadPartials = require('./lib/loadPartials');
-Panini.prototype.loadData = require('./lib/loadData');
-Panini.prototype.render = require('./lib/render');
+PaniniMustache.prototype.refresh = require('./lib/refresh');
+PaniniMustache.prototype.loadLayouts = require('./lib/loadLayouts');
+PaniniMustache.prototype.loadPartials = require('./lib/loadPartials');
+PaniniMustache.prototype.loadData = require('./lib/loadData');
+PaniniMustache.prototype.render = require('./lib/render');
 
 /**
  * Gulp stream function that renders HTML pages. The first time the function is invoked in the stream, a new instance of Panini is created with the given options.
@@ -34,7 +34,7 @@ Panini.prototype.render = require('./lib/render');
  */
 module.exports = function(options) {
   if (!panini) {
-    panini = new Panini(options);
+    panini = new PaniniMustache(options);
     panini.refresh();
     module.exports.refresh = panini.refresh.bind(panini);
   }
@@ -43,5 +43,5 @@ module.exports = function(options) {
   return panini.render();
 }
 
-module.exports.Panini = Panini;
+module.exports.PaniniMustache = PaniniMustache;
 module.exports.refresh = function() {}
